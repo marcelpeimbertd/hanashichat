badca1c5c6528017a0e422354dd3c1dbc775f48f
jmarcelfpg <jmarcel_fpg@hotmail.com>
2018-04-25 11:05:53 -0700

    Starting the server

---
+++ src/server/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.listen(3000, () => { console.log('server running on port 3000'); });
