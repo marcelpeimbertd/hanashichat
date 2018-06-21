22324b97bda969bb7b076e4179fe91ced5712f62
jmarcelfpg <jmarcel_fpg@hotmail.com>
2018-06-21 15:25:34 -0700

    styling the language settings

--- src/public/app/components/Settings/lenguage.tsx
+++ src/public/app/components/Settings/lenguage.tsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import { translate } from 'react-i18next';
 import { Options } from 'react-i18next/src/I18n';
+import './settings';
+
+interface ILanguage {
+    abbr: string;
+    txt: string;
+}
+type ILanguages = ILanguage[];
 
 class Lenguage extends React.Component<Options>{
+    private languages = [{
+        abbr: 'es',
+        txt: 'Español',
+    },
+    {
+        abbr: 'en',
+        txt: 'English',
+    },
+    ];
     public render() {
         const { i18n } = this.props;
         const changeLanguage = (lng: string) => {
             i18n.changeLanguage(lng);
         };
-        return <div>
-            <button onClick={() => changeLanguage('es')}>es</button>
-            <button onClick={() => changeLanguage('en')}>en</button>
-        </div>;
+        return <div className="language">
+            <select name="languageSelect" id="languageSelect"
+                className="options"
+                onChange={(event) => changeLanguage((event.target as HTMLSelectElement).value)}>
+                {this.createOptions(i18n.language, ...this.languages)}
+            </select>
+        </div >;
+    }
+    private createOptions = (current: string, ...languages: ILanguage[]) => {
+        if (languages.length) {
+            return languages.map(
+                (language) => <option value={language.abbr}
+                    selected={current === language.abbr}>
+                    {language.txt}
+                </option>);
+        }
     }
 }
 
